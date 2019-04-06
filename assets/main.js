bef38e6f81f162e6438b1d789197161990f68b1a
Mike Tatsuo <mike_tatsuo@hotmail.com>
2019-04-06 18:03:00 -0300

    flickr api, exibindo imagens

---
+++ assets/main.js
@@ -0,0 +1,34 @@
+---
+---
+
+$(document).ready(() => {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.flickr.com/services/rest/",
+        "method": "POST",
+        "headers": {
+          "Content-Type": "application/x-www-form-urlencoded"
+            },
+        "data": {
+          "method": "flickr.photos.getRecent",
+          "format": "json",
+          "api_key": "{{ site.env.FLICKR_API_KEY }}",
+          "privacy_key": 1,
+          "per_page": 6,
+          "nojsoncallback": 1
+        }
+      }
+      
+      $.ajax(settings).done((response) => {          
+        let photos = response.photos.photo;
+        let x = 0;
+        photos.forEach((photo, index) => {
+            if (!(index % 3)) {
+                x++;
+                $("#images").append(`<div class="row" id="row${x}"></div>`);
+            }
+            $(`#row${x}`).append(`<div id="col${index}" class="col-4"><img alt="${photo.id}" title="${photo.title}" src="https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_n.jpg"></div>`)
+        })
+      });
+});
--- index.md
+++ index.md
@@ -2,25 +2,4 @@
 layout: home
 title: Imagens recentes do flickr
 ---
-<div class="row">
-    <div class="col-4">
-        <img class="img-fluid" alt="img1" title="" src="">
-    </div>
-    <div class="col-4">
-        <img class="img-fluid" alt="img2" title="" src="">
-    </div>
-    <div class="col-4">
-        <img class="img-fluid" alt="img3" title="" src="">
-    </div>
-</div>
-<div class="row">
-    <div class="col-4">
-        <img class="img-fluid" alt="img4" title="" src="">
-    </div>
-    <div class="col-4">
-        <img class="img-fluid" alt="img5" title="" src="">
-    </div>
-    <div class="col-4">
-        <img class="img-fluid" alt="img6" title="" src="">
-    </div>
-</div>
+<div id="images"></div>
